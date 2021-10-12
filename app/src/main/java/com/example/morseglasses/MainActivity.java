696d38e2dc47136459a6aeab54355cdebac6e31c
Aliaa Asaad <aliaaasaad35@gmail.com>
2021-10-12 18:15:55 +0200

    second commit

--- app/src/main/java/com/example/morseglasses/MainActivity.java
+++ app/src/main/java/com/example/morseglasses/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.morseglasses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView paireddevicesList;
+BluetoothAdapter mBTadapter;
+String Tag="Bluetooth devices";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    @Override
+    protected void onResume(){
+super.onResume();
+//verify status bluetooth
+        VerifyStatusBT();
+        //ListView
+        //define List View
+        paireddevicesList=(ListView) findViewById(R.id.paireddevicesListView);
+
+        //data
+        mBTadapter =BluetoothAdapter.getDefaultAdapter();
+
+        Set <BluetoothDevice> devices=mBTadapter.getBondedDevices();
+        //Adapter بياخد اللاي اوت ويحطها علي الليست فيو
+        ArrayAdapter<String>mpairedArrayAdapter=new ArrayAdapter<String>(this,R.layout.devicesbt);
+        //Adapter - ListView
+        paireddevicesList.setAdapter(mpairedArrayAdapter);
+
+        for(BluetoothDevice x:devices){
+            mpairedArrayAdapter.add(x.getName()+"\n"+x.getAddress());
+
+        }
+
+        //click on any bluetooth device - secondactivty (address);
+        paireddevicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i=new Intent(MainActivity.this,SecondActivity.class);
+                //address
+                //17
+                //info=name+address
+                String info=((TextView) view).getText().toString();
+                //subString
+                String address=info.substring(info.length()-17);
+                i.putExtra("Extra_Address",address);
+                startActivity(i);
+            }
+        });
+    }
+    private void VerifyStatusBT(){
+        //Adapter - bluetooth
+        mBTadapter =BluetoothAdapter.getDefaultAdapter();
+        //if devices support bluetooth or not
+        if(mBTadapter==null)
+            Toast.makeText(getBaseContext(),"Your device don't support bluetooth",Toast.LENGTH_LONG).show();
+        else
+        {
+            if(mBTadapter.isEnabled()){
+                Log.d(Tag,"Bluetooth activated");
+            }
+            else{//intent
+                Intent i=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                onActivityResult(RESULT_OK,1,i);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/morseglasses/SecondActivity.java
+++ app/src/main/java/com/example/morseglasses/SecondActivity.java
@@ -2,13 +2,141 @@ package com.example.morseglasses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.logging.Handler;
+import java.util.logging.SocketHandler;
 
 public class SecondActivity extends AppCompatActivity {
+BluetoothAdapter mBTadapter;
+    BluetoothSocket btSocket;
+    StringBuilder sb= new StringBuilder();
+    String Tag="Bluetooth devices";
 
+    //serial port Bluetooth device
+public static final UUID My_UUID =UUID.fromString("00001101-0000-1000-8000-00805F9B34F0");
+public int Received_msg=1;
+String s;
+TextView dataTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_astivity);
+        mBTadapter =BluetoothAdapter.getDefaultAdapter();
+        dataTextView=(TextView) findViewById(R.id.dataTextView);
+        Handler h=new Handler(){
+            public void handlerMessage(android.os.Message msg){
+                switch (msg.what){
+                    case 1:
+                        byte [] readBUF=(byte[]) msg.obj;
+                        String strIncom=new String(readBUF,0,msg.arg1);
+                        sb.append(strIncom);
+                        //end_point
+                        int endOfLine=sb.indexOf("#");
+                        //substring
+                        String sbprint=sb.substring(0,endOfLine);
+                        //receive new data
+                        sb.delete(0,sb.length());
+s=s+""+sbprint;
+                        dataTextView.setText(sbprint);
+
+                }
+            }
+        };
+        VerifyBluetooth();
+    }
+
+
+    private void VerifyBluetooth(){
+        //Adapter - bluetooth
+        mBTadapter =BluetoothAdapter.getDefaultAdapter();
+        //if devices support bluetooth or not
+        if(mBTadapter==null)
+            Toast.makeText(getBaseContext(),"Your device don't support bluetooth",Toast.LENGTH_LONG).show();
+        else
+        {
+            if(mBTadapter.isEnabled()){
+                Log.d("Done","Bluetooth activated");
+            }
+            else{//intent
+                Intent i=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                onActivityResult(RESULT_OK,1,i);
+            }
+    }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+//address (port number)
+       String Address= getIntent().getStringExtra("Extra_Address");
+        BluetoothDevice device=mBTadapter.getRemoteDevice(Address);
+        //socket
+        try {
+            btSocket=createBluetoothSocket(device);
+            mBTadapter.cancelDiscovery();
+            btSocket.connect();
+            connectThread n=new connectThread(btSocket);
+            n.start() ;
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        //getmethod
+        return device.createRfcommSocketToServiceRecord(My_UUID);
+    }
+public class connectThread extends Thread{
+private InputStream mmInstream;
+private OutputStream mmOutstream;
+connectThread(BluetoothSocket socket){
+    try {
+        mmInstream= socket.getInputStream();
+        mmOutstream= socket.getOutputStream();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+}
+@Override
+    public void run(){
+    byte [] buffer=new byte[256];
+    int bytes;
+    //read data from arduino to mobile
+    try {
+       bytes= mmInstream.read(buffer);
+       //handler
+
+       //text view
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+    public void write(String msg){
+        byte[] msgbyte=msg.getBytes();
+        try {
+            mmOutstream.write(msgbyte);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+    public void clear (View v){
+        dataTextView.setText("");
     }
 }
